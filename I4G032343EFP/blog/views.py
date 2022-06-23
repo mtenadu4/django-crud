6c61f99c674d81f04466f58006d167d1aaf40136
mtenadu4 <mtenadu4@gmail.com>
2022-06-23 08:48:05 +0000

    First commit

---
+++ I4G032343EFP/blog/views.py
@@ -0,0 +1,34 @@
+from pathlib import PosixPath
+from typing import List
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from .models import Post
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
